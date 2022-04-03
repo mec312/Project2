ef3527ff253d7e1da8635880af9ddaeec8554417
mec312 <mec_312@hotmail.com>
2022-04-02 22:18:27 -0500

    Add CheckStyle

--- banking-user-service/src/main/java/com/api/bank/model/User.java
+++ banking-user-service/src/main/java/com/api/bank/model/User.java
@@ -2,15 +2,15 @@ package com.api.bank.model;
 
 import com.api.bank.internal.UserStatus;
 import com.api.bank.internal.UserType;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
-import org.springframework.validation.annotation.Validated;
-
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 @Builder
--- banking-user-service/src/main/java/com/api/bank/web/UserController.java
+++ banking-user-service/src/main/java/com/api/bank/web/UserController.java
@@ -8,10 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Mono;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
 
 import javax.validation.Valid;
 import java.util.HashMap;
