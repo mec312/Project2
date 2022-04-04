de038c1df855975d3fa87e75de07ee011f2d6b25
USERSAD\mcorrele <mcorrele@emeal.nttdata.com>
2022-04-04 05:37:38 -0500

    Yanki

---
+++ banking-yanki-service/src/main/java/com/api/bank/BankingYankiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.api.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingYankiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingYankiServiceApplication.class, args);
+	}
+
+}
