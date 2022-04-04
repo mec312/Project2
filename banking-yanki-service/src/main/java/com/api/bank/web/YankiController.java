6fc18d7d2235ba118b7bc309278fad79c9b4a1e6
USERSAD\mcorrele <mcorrele@emeal.nttdata.com>
2022-04-04 14:24:47 -0500

    no message

--- banking-yanki-service/src/main/java/com/api/bank/web/YankiController.java
+++ banking-yanki-service/src/main/java/com/api/bank/web/YankiController.java
@@ -6,10 +6,15 @@ import com.api.bank.model.User;
 import io.reactivex.Maybe;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -43,5 +48,15 @@ public class YankiController {
         return yankiService.MembershipYanki(yankiaccount, chosenaccount);
     }
 
-
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
 }
