0dec9c9a50260deac3d26bf22dc612342308d7aa
USERSAD\mcorrele <mcorrele@emeal.nttdata.com>
2022-04-04 08:16:57 -0500

    Controlador

--- banking-yanki-service/src/main/java/com/api/bank/web/YankiController.java
+++ banking-yanki-service/src/main/java/com/api/bank/web/YankiController.java
@@ -1,6 +1,7 @@
 package com.api.bank.web;
 
 import com.api.bank.bussiness.YankiService;
+import com.api.bank.model.Transaction;
 import com.api.bank.model.User;
 import io.reactivex.Maybe;
 import lombok.extern.slf4j.Slf4j;
@@ -21,30 +22,31 @@ public class YankiController {
     private YankiService yankiService;
 
     @PostMapping(value = "/regUserYanki")
-    public Maybe<ResponseEntity<User>> createUser(@Valid @RequestBody User request) {
+    public Maybe<ResponseEntity<User>> regUserYanki(@Valid @RequestBody User request) {
         log.info("Creating user with {}", request.toString());
         return yankiService.createUserYanki(request);
     }
 
     @PostMapping(value = "/paymentYanki")
-    public Maybe<ResponseEntity<Transaction>> PayTransaction(@Valid @RequestBody Transaction trx){
+    public Maybe<ResponseEntity<Transaction>> paymentYanki(@Valid @RequestBody Transaction trx){
         log.info("Pagos - Inicio");
-        return yankiService.PayTransaction(trx.getAmount(), trx.getFromAccount());
+        return yankiService.PayYanki(trx.getFromAccount(), trx.getAmount());
     }
 
     @PostMapping(value = "/fundTransferYanki")
-    public Maybe<ResponseEntity<Transaction>> FundTransferTransaction(@Valid @RequestBody Transaction trx) {
-        return yankiService.FundTransferTransaction(trx.getFromAccount(),
+    public Maybe<ResponseEntity<Transaction>> fundTransferYanki(@Valid @RequestBody Transaction trx) {
+        return yankiService.FundTransferYanki(trx.getFromAccount(),
                 trx.getToAccount(),
                 trx.getAmount());
     }
 
+    /*
     @PostMapping(value = "/membershipYanki")
-    public Maybe<ResponseEntity<Transaction>> FundTransferTransaction(@Valid @RequestBody Transaction trx) {
-        return yankiService.FundTransferTransaction(trx.getFromAccount(),
+    public Maybe<ResponseEntity<Transaction>> membershipYanki(@Valid @RequestBody Transaction trx) {
+        return yankiService.MembershipYanki(trx.getFromAccount(),
                 trx.getToAccount(),
                 trx.getAmount());
     }
-
+    */
 
 }
